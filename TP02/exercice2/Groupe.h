a3e526f692b351c7e06b64a3131e6128af54730c
Simon Zeru <simzeru@gmail.com>
2024-09-16 11:11:57 +0000

    Commentaires exercice 2

--- TP02/exercice2/Groupe.h
+++ TP02/exercice2/Groupe.h
@@ -4,13 +4,20 @@
 #include <string>
 #include <vector>
 #include "Personne.h"
-
+/*
+Forme canonique de coplien
+*/
 class Groupe {
 public:
+    // Constructeur par défaut
     Groupe(const std::string & intitule = "Un Groupe");
+    // Constructeur par recopie
     Groupe(const Groupe & other);
+    // Opérateur d'affectation
     Groupe & operator=(const Groupe & other);
+    // Destructeur
     ~Groupe();
+    // Libération mémoire
     void empty();
     void setIntitule(const std::string & intitule);
     void addPersonne(const std::string & nom);
