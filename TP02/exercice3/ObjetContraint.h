7110b031465d8f98dff0da90f12d23fe9906daaa
Simon <simzeru@gmail.com>
2024-09-18 16:59:52 +0200

    tests ObjetContraint.h

--- TP02/exercice3/ObjetContraint.h
+++ TP02/exercice3/ObjetContraint.h
@@ -12,7 +12,8 @@ class ObjetContraint {
         const T & getMax() const;
         const T & getVal() const;
         void setVal(const T & val);
-        const operator T() const;
+        operator T() const;
+        void afficher(std::ostream & sortie = std::cout) const;
     private:
         T m_val;
         T m_min;
@@ -22,18 +23,46 @@ class ObjetContraint {
 
 // A COMPLETER : Implémentez ici les méthodes du template ObjetContraint<T> (pas de fichier .cpp pour un template)
 template<class T>
-ObjetContraint<T>::ObjetContraint(T min, T max, T val): m_min(min), m_max(max), m_val(val) {}
+ObjetContraint<T>::ObjetContraint(T min, T max, T val): m_min(min), m_max(max), m_val(val) {
+    if (!(min < val && val < max)) {
+        throw std::out_of_range("valeur hors bornes");
+    }
+}
 
-template<class T> int ObjetContraint<T>::getMin() const {
+template<class T>
+const T & ObjetContraint<T>::getMin() const {
     return this->m_min;
 }
 
-template<class T> int ObjetContraint<T>::getMax() const {
+template<class T>
+const T & ObjetContraint<T>::getMax() const {
     return this->m_max;
 }
 
-template<class T> int ObjetContraint<T>::getVal() const {
+template<class T>
+const T & ObjetContraint<T>::getVal() const {
     return this->m_val;
 }
+template<class T>
+void ObjetContraint<T>::setVal(const T & val) {
+    if (!(m_min < val && val < m_max )) {
+        throw std::out_of_range("Valeur hors bornes");
+    }
+    this->m_val = val;
+}
+
+template<class T>
+ObjetContraint<T>::operator T() const {
+    return this->m_val;
+}
+
+template<class T>
+void ObjetContraint<T>::afficher(std::ostream &sortie) const {
+    std::cout << "Val : " << m_val << std::endl;
+    std::cout << "Min : " << m_min << std::endl;
+    std::cout << "Max : " << m_max << std::endl;
+
+}
+
 #endif /* OBJETCONTRAINT_H */
 
--- TP02/exercice3/exercice3.cpp
+++ TP02/exercice3/exercice3.cpp
@@ -1,12 +1,66 @@
+#include <iostream>
+#include <stdexcept>
 #include "ObjetContraint.h"
 #include "Point.h"
-#include <iostream>
+
 using namespace std;
 
-int main(int argc, char** argv) {
+void testConstructeurValide() {
+    try {
+        ObjetContraint<int> obj(0, 10, 5);
+        if (obj.getVal() == 5 && obj.getMin() == 0 && obj.getMax() == 10) {
+            cout << "testConstructeurValide: PASS\n";
+        } else {
+            cout << "testConstructeurValide: FAIL\n";
+        }
+    } catch (...) {
+        cout << "testConstructeurValide: FAIL (exception levée)\n";
 
-    // A COMPLETER : Testez ici le template ObjetContraint<T>
+    }
+}
+
+void testConstructeurHorsBornes() {
+    try {
+        ObjetContraint<int> obj(0, 10, 15);  // Devrait lever une exception
+        cout << "testConstructeurHorsBornes: FAIL\n";
+    } catch (const out_of_range&) {
+        cout << "testConstructeurHorsBornes: PASS\n";
+    } catch (...) {
+        cout << "testConstructeurHorsBornes: FAIL (autre exception levée)\n";
+    }
+}
+
+void testSetValValide() {
+    try {
+        ObjetContraint<int> obj (0, 10, 7);
+        if (obj.getVal() == 7) {
+            cout << "testSetValValide: PASS\n";
+        } else {
+            cout << "testSetValValide: FAIL\n";
+        }
+    } catch (...) {
+        cout << "testSetValValide: FAIL (exception levée)\n";
+    }
+}
+
+void testSetValHorsBornes() {
+    try {
+        ObjetContraint<int> obj(0, 10, 7);
+        obj.setVal(15);  // Devrait lever une exception
+        cout << "testSetValHorsBornes: FAIL\n";
+    } catch (const out_of_range&) {
+        cout << "testSetValHorsBornes: PASS\n";
+    } catch (...) {
+        cout << "testSetValHorsBornes: FAIL (autre exception levée)\n";
+    }
+}
+
+int main() {
+
+    testConstructeurValide();
+    testConstructeurHorsBornes();
+    testSetValValide();
+    testSetValHorsBornes();
 
     return 0;
 }
-
