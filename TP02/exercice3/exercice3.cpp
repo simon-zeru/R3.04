935fe57f7bc99e8cd078ae246352bf83dc499703
simon-zeru <sim.zeru@gmail.com>
2024-09-19 09:05:13 +0200

    Instance du template ObjetContraint<T> avec la classe Point

--- TP02/exercice3/exercice3.cpp
+++ TP02/exercice3/exercice3.cpp
@@ -63,9 +63,30 @@ int main() {
 //    testSetValHorsBornes();
 
     Point p1;
-    Point * p2;
+    Point p2;
+    Point p3;
 
+    cout << "Saisir un point p1 : " << endl;
     p1.saisir();
+    p1.afficher();
+    do {
+        cout << "Saisir un point p2 >= p1 : " << endl;
+        p2.saisir();
+        p2.afficher();
+
+        if (p2<p1) {
+            cout << "Le point p2 est inférieur au point p1, réessayer..." << endl;
+        }
+    } while (p2<p1);
+
+    cout << "Saisir un point p3 : " << endl;
+    p3.saisir();
+    try {
+        ObjetContraint<Point> o1 (p1, p2, p3);
+        cout << "p3 se trouve dans le rectangle délimité par p1 et p2" << endl;
+    } catch (const out_of_range&) {
+        cout << "Point p3 hors du rectangle délimité par p1 et p2" << endl;
+    }
 
     return 0;
 }
