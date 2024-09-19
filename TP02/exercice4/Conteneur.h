82932b685547fab1cda9be2eea4ba92a4502f4b6
simon-zeru <sim.zeru@gmail.com>
2024-09-19 09:55:38 +0200

    Exercice 4

--- TP02/exercice4/Conteneur.h
+++ TP02/exercice4/Conteneur.h
@@ -9,10 +9,76 @@ template<class T>
 class Conteneur {
 
 // A COMPLETER : Spécifiez ici le template Conteneur<T>
-
+    public:
+        Conteneur();
+        ~Conteneur();
+        Conteneur(const Conteneur<T> & unConteneur);
+        Conteneur<T> & operator=(const Conteneur<T> & unConteneur);
+        void empty();
+        void ajouter(T* element);
+        void afficher(std::ostream & out = std::cout) const;
+    private:
+    std::vector<T*> contenu;
 };
 
 // A COMPLETER : Implémentez ici les méthodes du template Conteneur<T> (pas de fichier .cpp pour un template)
+template<class T>
+Conteneur<T>::Conteneur() {
+    // les classe standards de c++ s'initialise par défaut,
+    // on aurait pu faire : contenu() avant l'accolade
+}
+
+template<class T>
+Conteneur<T> &Conteneur<T>::operator=(const Conteneur<T> &unConteneur) {
+    if (this != &unConteneur) {
+        this->empty();
+        for (T* element : unConteneur.contenu) {
+            contenu->push_back(element);
+        }
+    }
+    return *this;
+}
+
+template<class T>
+Conteneur<T>::Conteneur(const Conteneur<T> &unConteneur) {
+    for (T* element : unConteneur.contenu) {
+        contenu.push_back(element);
+    }
+}
+
+template<class T>
+Conteneur<T>::~Conteneur() {
+    this->empty();
+}
+
+template<class T>
+void Conteneur<T>::empty() {
+    for (T* element : contenu) {
+        delete element;
+    }
+    contenu.clear();
+}
+
+template<class T>
+void Conteneur<T>::ajouter(T *element) {
+    contenu->push_back(element);
+}
+
+template<class T>
+void Conteneur<T>::afficher(std::ostream &out) const {
+
+    out << "Conteneur:\n";
+    for (T* element : contenu) {
+        out << *element << " ";
+    }
+    out << std::endl;
+
+}
+
+
+
+
+
 
 
 #endif /* CONTENEUR_H */
--- TP02/exercice4/exercice4.cpp
+++ TP02/exercice4/exercice4.cpp
@@ -7,6 +7,31 @@ using namespace std;
 int main(int argc, char** argv) {
 
     // A COMPLETER : tester le template Conteneur<T>
-    
-    return 0;
+    Conteneur<string> c1;
+
+    // Si des arguments sont passés, on les ajoute au conteneur
+    for (int i = 1; i < argc; ++i) {
+        c1.ajouter(new string(argv[i]));  // Ajouter chaque argument au conteneur
+    }
+
+    // Afficher les éléments ajoutés depuis les arguments
+    c1.afficher();
+
+    // Copier le conteneur
+    Conteneur<string> c2 = c1;
+    cout << "Conteneur copié :" << endl;
+    c2.afficher();
+
+    // Demander à l'utilisateur de choisir un index
+    cout << "Choisissez un index (entre 0 et " << argc - 2 << ") : ";
+    int index;
+    cin >> index;
+
+    if (index >= 0 && index < argc - 1) {
+        cout << "Vous avez choisi: " << *c1.contenu.at(index) << endl;
+    } else {
+        cout << "Index invalide!" << endl;
+    }
+
+    return 0;;
 }
